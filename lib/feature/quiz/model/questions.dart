703a981cb3692d49d6832ee9d26a20dc5c8bbf94
Emir Bayraktar <96651996+Bayrktr@users.noreply.github.com>
2023-11-15 19:52:01 +0300

    commit

---
+++ lib/feature/quiz/model/questions.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'answers.dart';
+
+class Questions {
+  String? id;
+  String? questionTitle;
+  String? question;
+  Timestamp? date; // Değişiklik burada
+  Answers? answers;
+  int? correctQuestionIndex;
+
+  Questions({
+    this.id,
+    this.questionTitle,
+    this.question,
+    this.date,
+    this.answers,
+    this.correctQuestionIndex,
+  });
+
+  Questions.fromJson(Map<String, dynamic> json) {
+    id = json['id'] as String?;
+    questionTitle = json['questionTitle'] as String?;
+    question = json['question'] as String?;
+    date = json['date'] as Timestamp?; // Değişiklik burada
+    answers = json['answers'] != null ? Answers.fromJson(json['answers']) : null;
+    correctQuestionIndex = json['correctQuestionIndex'] as int?;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['id'] = id;
+    data['questionTitle'] = questionTitle;
+    data['question'] = question;
+    data['date'] = date; // Değişiklik burada
+    if (answers != null) {
+      data['answers'] = answers!.toJson();
+    }
+    data['correctQuestionIndex'] = correctQuestionIndex;
+    return data;
+  }
+
+  Questions copyWith({
+    String? id,
+    String? questionTitle,
+    String? question,
+    Timestamp? date,
+    Answers? answers,
+    int? correctQuestionIndex,
+  }) {
+    return Questions(
+      id: id ?? this.id,
+      questionTitle: questionTitle ?? this.questionTitle,
+      question: question ?? this.question,
+      date: date ?? this.date,
+      answers: answers ?? this.answers,
+      correctQuestionIndex: correctQuestionIndex ?? this.correctQuestionIndex,
+    );
+  }
+}
