0c8dcc0c25e59eba92bd275a22e004a1d9153a30
Emir Bayraktar <96651996+Bayrktr@users.noreply.github.com>
2023-12-23 13:15:01 +0300

    quiz view soru geçişleri tamamlandı

--- lib/feature/top_picks/top_picks_view.dart
+++ lib/feature/top_picks/top_picks_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kartal/kartal.dart';
+import 'package:riverpod_architecture/feature/top_picks/components/top_pick_container.dart';
+import 'package:riverpod_architecture/product/navigation/enum/router_items.dart';
 import '../../product/constants/color_constants.dart';
 import '../../product/constants/image_constants.dart';
 import '../../product/constants/string_constants.dart';
@@ -40,57 +42,15 @@ class _TopPicksContainers extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: const ClampingScrollPhysics(),
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: 4,
-        physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              Container(
-                height: context.sized.dynamicHeight(0.2),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(ImageConstants.homeIcon.toImage),
-                    fit: BoxFit.cover,
-                  ),
-                  color: ColorConstants.smootWhite.getColor,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                padding: context.padding.normal,
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      StringConstants.quiz,
-                      style: GoogleFonts.baloo2(
-                        color: ColorConstants.smootWhite.getColor,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: context.sized.normalValue,
-                        vertical: context.sized.lowValue,
-                      ),
-                      decoration: BoxDecoration(
-                        color: ColorConstants.white.getColor.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: Icon(
-                        Icons.arrow_right_alt,
-                        color: ColorConstants.white.getColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(padding: context.padding.onlyTopLow),
-            ],
-          );
-        },
+      child: Column(
+        children: [
+          TopPickContainer(
+              backgroundImage:
+                  AssetImage(ImageConstants.homePageTop1.toImage),
+              title: 'Quiz',
+              navigatePage: RouterItems.leaderboard),
+          Padding(padding: context.padding.onlyTopLow),
+        ],
       ),
     );
   }
